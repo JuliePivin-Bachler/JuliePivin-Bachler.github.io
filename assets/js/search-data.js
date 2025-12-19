c035a289fdf9dca0180ddff1999068da2164218c
JuliePivin-Bachler <59390006+JuliePivin-Bachler@users.noreply.github.com>
2025-12-19 11:15:38 +0000

    Deploying to gh-pages from @ JuliePivin-Bachler/JuliePivin-Bachler.github.io@ffc11b6dba7f2d71a88e8eb330ec44c2da91661d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "Website under construction...",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
