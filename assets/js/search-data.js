464dc857158c8b407be87c361b7448635af0983a
JuliePivin-Bachler <59390006+JuliePivin-Bachler@users.noreply.github.com>
2025-12-19 13:13:02 +0000

    Deploying to gh-pages from @ JuliePivin-Bachler/JuliePivin-Bachler.github.io@0d5bb15f5f2ba1e397eda61222caa2a7220335af 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-contact",
           title: "Contact",
-          description: "‘Alone we can do so little. Together we can do so much’ - Helen Keller. So, do not hesitate to get in touch!",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/contact/";
