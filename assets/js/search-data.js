fe4f810376ca98b0c09fbd1c60620a4542e21e78
JuliePivin-Bachler <59390006+JuliePivin-Bachler@users.noreply.github.com>
2025-12-21 16:20:41 +0000

    Deploying to gh-pages from @ JuliePivin-Bachler/JuliePivin-Bachler.github.io@851191cfcf77b2a898443d6a4eaf4881c97459f6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "Teaching",
-          description: "‘What’s the point of even having a world if you’re not going to pass it on?’ - Project Hail Mary, Andy Weir. Here, you can explore all the activaties related to teaching and mentoring that I have undertaken to pass on knowledge. Website under construction...",
+          description: "‘What’s the point of even having a world if you’re not going to pass it on?’ - Project Hail Mary, Andy Weir. Here, you can explore all the activaties related to teaching and mentoring that I have undertaken to pass on knowledge.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
